60157e3f38ccf7cd6b75d0bc55780e2b7688052a
Pawan Gupta <pawangupta@gmail.com>
2021-10-02 05:15:59 -0700

    Added Hour_glass_pattern and right_star_pattern

---
+++ Patterns/reverse_pyramid_pattern.py
@@ -0,0 +1,11 @@
+def pattern(n):
+      k = 2*n -2
+      for i in range(n,-1,-1):
+           for j in range(k,0,-1):
+                print(end=" ")
+           k = k +1
+           for j in range(0, i+1):
+                print("*", end=" ")
+           print("&#92r")
+ 
+pattern(5)
