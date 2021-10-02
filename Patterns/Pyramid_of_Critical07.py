22cd3064884b82679819077754c8b2eefb392241
Shubhit Sardana <91517488+Critical07@users.noreply.github.com>
2021-10-03 01:34:17 +0530

    Update Pyramid_of_Critical07.py

--- Patterns/Pyramid_of_Critical07.py
+++ Patterns/Pyramid_of_Critical07.py
@@ -1,5 +1,24 @@
+CODE
+
 str1 = "Critical07"  
 x = ""  
 for i in str1:  
     x += i  
     print(x) 
+
+
+    
+    
+OUTPUT
+
+
+C
+Cr
+Cri
+Crit
+Criti
+Critic
+Critica
+Critical
+Critical0
+Critical07
