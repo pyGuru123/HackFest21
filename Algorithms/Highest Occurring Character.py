af9bb7be07b3c967aa0ab88c4c7deb25dadec314
Khushi Katiyar <91772262+khuushii17@users.noreply.github.com>
2021-10-02 22:21:53 +0530

    Create Highest Occurring Character.py

---
+++ Algorithms/Highest Occurring Character.py
@@ -0,0 +1,18 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+def get_max_occuring_char(str1):
+  ASCII_SIZE = 256
+  ctr = [0] * ASCII_SIZE
+  max = -1
+  ch = ''
+  for i in str1:
+    ctr[ord(i)]+=1;
+ 
+  for i in str1:
+    if max < ctr[ord(i)]:
+      max = ctr[ord(i)]
+      ch = i
+  return ch
+
+str1=input()
+print(get_max_occuring_char(str1))
