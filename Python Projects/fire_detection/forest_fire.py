e26c62d0841ae74018c9f4e7b5cfd667876a8be3
adipurnamk <muhammad.adipurna.k@gmail.com>
2021-10-03 05:57:37 +0700

    added fire image detection project with sample images

---
+++ Python Projects/fire_detection/forest_fire.py
@@ -0,0 +1,62 @@
+"""
+File: forestfire.py
+----------------
+This program highlights fires in an image by identifying
+pixels who red intensity is more than INTENSITY_THRESHOLD times
+the average of the red, green, and blue values at a pixel.
+Those "sufficiently red" pixels are then highlighted in the
+image and the rest of the image is turned grey, by setting the
+pixels red, green, and blue values all to be the same average
+value.
+"""
+
+
+# The line below imports SimpleImage for use here
+# Its depends on the Pillow package being installed
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/greenland-fire.png'
+
+def find_flames(filename):
+    """
+    This function should highlight the "sufficiently red" pixels
+    in the image and grayscale all other pixels in the image
+    in order to highlight areas of wildfires.
+    """
+    image = SimpleImage(filename)
+    for y in range(image.height):
+        for x in range(image.width):
+            pixel = image.get_pixel(x, y)
+            pixel_avg = (pixel.red + pixel.green + pixel.blue) / 3
+            if pixel.red >= pixel_avg:
+                pixel.red = 255
+            else:
+                pixel.red = pixel_avg
+                pixel.green = pixel_avg
+                pixel.blue = pixel_avg
+    return image
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original fire
+    original_fire = SimpleImage(filename)
+    original_fire.show()
+
+    # Show the highlighted fire
+    highlighted_fire = find_flames(filename)
+    highlighted_fire.show()
+
+    
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
