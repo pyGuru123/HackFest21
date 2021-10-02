1f99345cb0e13034e3ff545fed25e7cf59d396b3
shubh asati <74060169+shubhtrikal@users.noreply.github.com>
2021-10-02 16:43:36 +0530

    please approve this script with tag

---
+++ trial.py
@@ -0,0 +1,25 @@
+print("Hello")
+
+n = int(input())
+
+if(n > 0):
+    print(f"{n} is positive")
+    print(f"{n} may be zero")
+
+else:
+    print(f"{n} is not positive")
+
+
+    print(f"hey")
+
+
+
+for each in range(6):
+    print(each)
+    print(each/2)
+
+
+def call(x):
+    print(x)
+
+call(-5)
