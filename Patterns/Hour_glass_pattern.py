cdc2ebd869303cad7e0df4d0b5f80a53b6782cd1
Tausiq <tausiqsamantaray03@gmail.com>
2021-10-03 20:18:17 +0530

    Error solved

--- Patterns/Hour_glass_pattern.py
+++ Patterns/Hour_glass_pattern.py
@@ -8,13 +8,12 @@ def pattern(n):
                print("* " , end="")
           print(" ")
      k = 2*n - 2
-     for i in range(0 , n+1):
-          for j in range(0 , k):
-               print(end="")
+     for i in range(1 , n+1):
+          for j in range(1 , k):
+               print(end=" ")
           k = k - 1
           for j in range(0, i + 1):
                print("* ", end="")
           print(" ")
 
 pattern(5)
-#error issue to be opened
