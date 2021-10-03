088f4e3dac14e1a9291982a4120d5574a25bd765
Shivam Singh <shivamashish123@gmail.com>
2021-10-03 12:46:21 +0530

    Create pyramid_pattern.py
    Python program for pyramid pattern.

---
+++ pyramid_pattern.py
@@ -0,0 +1,25 @@
+def pypart2(n):
+     
+    # number of spaces
+    k = 2*n - 2
+ 
+    # outer loop to handle number of rows
+    for i in range(0, n):
+     
+        # inner loop to handle number spaces
+        # values changing acc. to requirement
+        for j in range(0, k):
+            print(end=" ")
+     
+        # decrementing k after each loop
+        k = k - 2
+     
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+         
+            # printing stars
+            print("* ", end="")
+     
+        # ending line after each row
+        print("\r")
