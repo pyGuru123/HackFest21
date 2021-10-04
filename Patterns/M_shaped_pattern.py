18581ff298213a87e88f68dea76a3afebece99a3
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2021-10-04 18:49:10 +0530

    M-shaped-pattern
    Hi sir, I have made the M shape pattern program. Kindly add it to your Repository.

---
+++ Patterns/M_shaped_pattern.py
@@ -0,0 +1,9 @@
+result_str="";    
+for row in range(0,7):    
+    for column in range(0,7):     
+        if (column == 1 or column == 5 or (row == 2 and (column == 2 or column == 4)) or (row == 3 and column == 3)):  
+            result_str=result_str+"* "    
+        else:      
+            result_str=result_str+"  "    
+    result_str=result_str+"\n"    
+print(result_str);
