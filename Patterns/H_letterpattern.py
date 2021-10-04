dccc602bed16fae5dc1a3f0db51c0bdc1b8b5323
Harshithardd <79263383+Harshithardd@users.noreply.github.com>
2021-10-04 22:34:40 +0530

    Update and rename H_letterpattern.py to Z_letterpattern.py

--- Patterns/H_letterpattern.py
+++
@@ -1,8 +0,0 @@
-#printing pattern of "H"
-for i in range(7):
-    for j in range(5):
-        if j==0 or j==4 or (i==3 and (j>0 and j<4)):
-            print("*",end="")
-        else:
-            print(end=" ")
-    print()
