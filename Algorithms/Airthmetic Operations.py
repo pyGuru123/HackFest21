b2f4428b4322519a04d0b6d22e0934ec36c3f4c8
Sumit Kumar <messikumar66@gmail.com>
2021-10-04 12:52:05 +0530

    Create Airthmetic Operations.py

---
+++ Algorithms/Airthmetic Operations.py
@@ -0,0 +1,22 @@
+'''
+Author : Sumit Kumar
+'''
+
+#airthmetic operations in python
+n, m = input().split()
+n = int(n)
+m = int(m)
+#addition : 
+print("Addition : ", n + m)
+
+#multiplication 
+print("Multiplication : ", n * m)
+
+#subtraction 
+print("Subtraction : ", n - m)
+
+#Division
+print("Division : ", n/m)
+
+#Power
+print("Power ", n, "^", m, " : ", n ** m)
