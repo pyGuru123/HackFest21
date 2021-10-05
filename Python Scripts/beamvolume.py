cfcc6d582599dd00ae98694f1a45491e0592b012
Noklent Fardian <noklent320@gmail.com>
2021-10-05 19:26:42 +0700

    make beam volume

---
+++ Python Scripts/beamvolume.py
@@ -0,0 +1,8 @@
+print ("PROGRAM PYTHON MENGHITUNG VOLUME BALOK")
+p = float(input("Panjang = "))
+l = float(input("Lebar   = "))
+t = float(input("Tinggi  = "))
+
+v = p*l*t
+
+print ("Volume Balok = %0.2f" %v)
