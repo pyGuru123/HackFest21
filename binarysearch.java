ea68daf02853bd13265558c857ac5b7c8fd0afb6
Divyagupta29 <89536179+Divyagupta29@users.noreply.github.com>
2021-10-07 18:19:22 +0530

    Create binarysearch.java
    binary search in java using the iterative method .

---
+++ binarysearch.java
@@ -0,0 +1,17 @@
+public int runBinarySearchIteratively(
+  int[] sortedArray, int key, int low, int high) {
+    int index = Integer.MAX_VALUE;
+    
+    while (low <= high) {
+        int mid = low  + ((high - low) / 2);
+        if (sortedArray[mid] < key) {
+            low = mid + 1;
+        } else if (sortedArray[mid] > key) {
+            high = mid - 1;
+        } else if (sortedArray[mid] == key) {
+            index = mid;
+            break;
+        }
+    }
+    return index;
+}
